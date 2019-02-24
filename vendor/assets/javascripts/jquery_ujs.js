3f1a3446ad84799d9f951a03841206f339f40f32
arvindfundly <arvind@fundly.com>
2019-02-24 23:38:23 +0530

    Update jquery_ujs.js

--- vendor/assets/javascripts/jquery_ujs.js
+++ vendor/assets/javascripts/jquery_ujs.js
@@ -12,9 +12,9 @@
 
   // Cut down on the number of issues from people inadvertently including jquery_ujs twice
   // by detecting and raising an error when it happens.
-  if ( $.rails !== undefined ) {
-    $.error('jquery-ujs has already been loaded!');
-  }
+  //if ( $.rails !== undefined ) {
+  //  $.error('jquery-ujs has already been loaded!');
+  //}
 
   // Shorthand to make it a little easier to call public rails functions from within rails.js
   var rails;
